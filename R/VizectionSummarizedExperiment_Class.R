bf55b0dc1cd3a3f1ad7c4c8df5e2e22446d06af3
Simon Besson-Girard <simon@besson-girard.fr>
2017-10-29 21:01:34 +0100

    Added constructor for Class
    * vizectionSummarizedExperiment() as constructor
    * corrected typo in VizectionSummarizedExperiment Class definition

--- R/VizectionSummarizedExperiment_Class.R
+++ R/VizectionSummarizedExperiment_Class.R
@@ -1,3 +1,5 @@
+# Class definition -------------------------------------------------------------
+
 setOldClass("dendrogram")
 setOldClass("pca")
 setOldClass("coa")
@@ -7,6 +9,31 @@ setClass(
   representation = representation(
     dendrogram = "dendrogram",
     pca = "pca",
-    ca = "coa"),
+    coa = "coa"),
   contains = "SummarizedExperiment"
 )
+
+# Constructor ------------------------------------------------------------------
+
+vizectionSummarizedExperiment <- function(se){
+  if(class(se) == "SummarizedExperiment"){
+    initialDendrogram <- FALSE
+    initialPca <- FALSE
+    initialCoa <- FALSE
+    class(initialDendrogram) <- "dendrogram"
+    class(initialPca) <- "pca"
+    class(initialCoa) <- "coa"
+    new(Class = "VizectionSummarizedExperiment",
+        dendrogram = initialDendrogram,
+        pca = initialPca,
+        coa = initialCoa,
+        colData = colData(se),
+        assays = se@assays,
+        NAMES = names(se),
+        elementMetadata = elementMetadata(se),
+        metadata = metadata(se)
+    )
+  } else{
+    stop("Data provided is not of Class 'SummarizedExperiment'.")
+  }
+}
